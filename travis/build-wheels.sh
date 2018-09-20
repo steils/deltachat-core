79ac44cf49b7a781cd3c480fdb169c652a711289
Floris Bruynooghe <flub@google.com>
2018-09-20 21:15:43 +0200

    Experiment with building manylinux1 wheels

---
+++ travis/build-wheels.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e -x
+
+# Install a system package required by our library
+yum install -y openssl-devel sqlite3-devel zlib-devel libsals-devel bzip2-devel
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install cffi
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install deltachat --no-index -f /io/wheelhouse
+    #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+done
