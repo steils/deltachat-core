d27ed10ca3576669198637324602a51ca338c162
Floris Bruynooghe <flub@google.com>
2018-09-23 14:37:13 +0200

    Some WIP
    This really isn't final yet.  It should probably build a recent
    openssl and other dependencies from scratch.  Who knows.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,14 +2,24 @@
 set -e -x
 
 # Install a system package required by our library
-yum install -y openssl-devel sqlite3-devel zlib-devel libsals-devel bzip2-devel
+yum install -y openssl-devel bzip2-devel
+
+# Install meson and ninja
+/opt/python/cp37-cp37m/bin/pip install meson
+pushd /usr/bin && ln -s /opt/_internal/cpython-3.7.0/bin/meson && popd
+# curl -O https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
+# unzip ninja-linux.zip
+# mv ninja /usr/bin/ninja
+cp /io/ninja /usr/bin/ninja
+
+# Build the library
+meson -Dmonolith=true /builddir /io
+ninja -C /builddir
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install cffi
-    pushd python
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-    popd
+    "${PYBIN}/pip" wheel /io/python -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
